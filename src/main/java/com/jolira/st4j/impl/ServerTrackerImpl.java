4d36a95e3f435aa44a31aaa157fe19c5b3e3c64d
Joachim Kainz <jfk@jolira.com>
2011-08-14 14:43:20 -0700

    submtting to the server successfully.

--- src/main/java/com/jolira/st4j/impl/ServerTrackerImpl.java
+++ src/main/java/com/jolira/st4j/impl/ServerTrackerImpl.java
@@ -5,13 +5,27 @@
 
 package com.jolira.st4j.impl;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.Map;
+import java.util.concurrent.Executor;
 
 import javax.inject.Inject;
 import javax.inject.Named;
 import javax.inject.Singleton;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.gson.Gson;
 import com.jolira.st4j.Metric;
 import com.jolira.st4j.ServerTracker;
 
@@ -24,6 +38,61 @@ import com.jolira.st4j.ServerTracker;
  */
 @Singleton
 public class ServerTrackerImpl implements ServerTracker {
+    final static Logger LOG = LoggerFactory.getLogger(ServerTrackerImpl.class);
+    private final static ThreadLocal<Map<String, Object>> localMetrics = new ThreadLocal<Map<String, Object>>() {
+        @Override
+        protected Map<String, Object> initialValue() {
+            return new HashMap<String, Object>();
+        }
+    };
+
+    private final static ThreadLocal<Map<String, Map<String, Object>>> local = new ThreadLocal<Map<String, Map<String, Object>>>() {
+        @Override
+        protected Map<String, Map<String, Object>> initialValue() {
+            return new HashMap<String, Map<String, Object>>();
+        }
+    };
+
+    static final void add(final String metric, final String key, final Object value) {
+        final Map<String, Map<String, Object>> map = local.get();
+        Map<String, Object> valByKey = map.get(metric);
+
+        if (valByKey == null) {
+            valByKey = new HashMap<String, Object>();
+
+            map.put(metric, valByKey);
+        }
+
+        valByKey.put(key, value);
+    }
+
+    private static String getDefaultMetricName(final Class<?> type) {
+        final String name = type.getName();
+
+        return name.toLowerCase();
+    }
+
+    final static <T> T getLocalMetric(final Class<T> type) {
+        final String metricName = getMetricName(type);
+        final Map<String, Object> _metricByName = localMetrics.get();
+        final Object o = _metricByName.get(metricName);
+
+        if (o != null) {
+            return type.cast(o);
+        }
+
+        final MetricBean<T> bean = new MetricBean<T>(type) {
+            @Override
+            protected void add(final String name, final Object value) {
+                ServerTrackerImpl.add(metricName, name, value);
+            }
+        };
+
+        _metricByName.put(metricName, bean.metric);
+
+        return bean.metric;
+    }
+
     private static String getMetricName(final Class<?> type) {
         final Metric anno = type.getAnnotation(Metric.class);
 
@@ -40,28 +109,54 @@ public class ServerTrackerImpl implements ServerTracker {
         return value;
     }
 
-    private static String getDefaultMetricName(final Class<?> type) {
-        final String name = type.getName();
+    private final Collection<Map<String, Map<String, Object>>> pending = new LinkedList<Map<String, Map<String, Object>>>();
 
-        return name.toLowerCase();
-    }
+    private final Executor executor;
 
-    private final ThreadLocal<Map<String, Object>> metricCache = new ThreadLocal<Map<String, Object>>() {
-        @Override
-        protected Map<String, Object> initialValue() {
-            return new HashMap<String, Object>();
-        }
-    };
-    private final ThreadLocal<Map<String, Map<String, Object>>> collected = new ThreadLocal<Map<String, Map<String, Object>>>() {
-        @Override
-        protected Map<String, Map<String, Object>> initialValue() {
-            return new HashMap<String, Map<String, Object>>();
-        }
-    };
+    private final URL url;
+
+    private boolean dispatcherRunning = false;
 
     @Inject
-    ServerTrackerImpl(@Named("ServerTrackerServer") final String server) {
-        //
+    ServerTrackerImpl(@Named("ServerTrackerServer") final String server, final Executor executor) {
+        this.executor = executor;
+
+        try {
+            url = new URL("http://" + server + "/submit/metric");
+        } catch (final MalformedURLException e) {
+            throw new Error("invalid server " + server, e);
+        }
+    }
+
+    private boolean dispatch(final Gson gson) throws IOException {
+        final Collection<Map<String, Map<String, Object>>> _pending = retrievePending();
+
+        if (_pending == null) {
+            return false;
+        }
+
+        final HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+        conn.setRequestProperty("Content-Type", "application/json");
+        conn.setDoOutput(true);
+        conn.setDoInput(true);
+
+        final OutputStream os = conn.getOutputStream();
+        final OutputStreamWriter wr = new OutputStreamWriter(os);
+
+        try {
+            gson.toJson(_pending, wr);
+        } finally {
+            wr.close();
+        }
+
+        final int status = conn.getResponseCode();
+
+        if (status != 200) {
+            LOG.error("{} response code while submitting {}", Integer.valueOf(status), gson.toJson(_pending));
+        }
+
+        return true;
     }
 
     /**
@@ -69,48 +164,81 @@ public class ServerTrackerImpl implements ServerTracker {
      * 
      * @return the values
      */
-    final Map<String, Map<String, Object>> getCollected() {
-        return collected.get();
+    final Map<String, Map<String, Object>> getLocal() {
+        return local.get();
     }
 
     @Override
     public <T> T getMetric(final Class<T> type) {
-        final String metricName = getMetricName(type);
-        final Map<String, Object> _metricByName = metricCache.get();
-        final Object o = _metricByName.get(metricName);
-
-        if (o != null) {
-            return type.cast(o);
-        }
-
-        final MetricBean<T> bean = new MetricBean<T>(type) {
-            @Override
-            protected void add(final String name, final Object value) {
-                ServerTrackerImpl.this.add(metricName, name, value);
-            }
-        };
-
-        _metricByName.put(metricName, bean.metric);
-
-        return bean.metric;
+        return getLocalMetric(type);
     }
 
-    final void add(final String metric, final String key, final Object value) {
-        final Map<String, Map<String, Object>> map = collected.get();
-        Map<String, Object> valByKey = map.get(metric);
+    void post(final Map<String, Map<String, Object>> _collected) {
+        synchronized (pending) {
+            pending.add(_collected);
 
-        if (valByKey == null) {
-            valByKey = new HashMap<String, Object>();
+            if (dispatcherRunning) {
+                return;
+            }
 
-            map.put(metric, valByKey);
+            dispatcherRunning = true;
         }
 
-        valByKey.put(key, value);
+        try {
+            final Gson gson = new Gson();
+
+            while (dispatch(gson)) {
+                // nothing
+            }
+        } catch (final IOException e) {
+            LOG.error("exception while dispatching", e);
+        } finally {
+            synchronized (pending) {
+                dispatcherRunning = false;
+            }
+        }
+    }
+
+    private Collection<Map<String, Map<String, Object>>> retrievePending() {
+        synchronized (pending) {
+            final int size = pending.size();
+
+            if (size < 1) {
+                return null;
+            }
+
+            final Collection<Map<String, Map<String, Object>>> _pending = new ArrayList<Map<String, Map<String, Object>>>(
+                    size);
+
+            _pending.addAll(pending);
+            pending.clear();
+
+            return _pending;
+        }
     }
 
     @Override
     public void submit() {
-        // TODO Auto-generated method stub
-    }
+        final Map<String, Map<String, Object>> _collected = getLocal();
 
+        local.remove();
+
+        final int size = _collected.size();
+
+        if (size < 1) {
+            return;
+        }
+
+        executor.execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    post(_collected);
+                } catch (final Throwable e) {
+                    LOG.error("error while submitting data", e);
+                }
+            }
+
+        });
+    }
 }
--- src/test/java/com/jolira/st4j/impl/ServerTrackerImplTest.java
+++ src/test/java/com/jolira/st4j/impl/ServerTrackerImplTest.java
@@ -2,15 +2,31 @@ package com.jolira.st4j.impl;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertSame;
+import static org.junit.Assert.fail;
 
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.Map;
+import java.util.concurrent.Executor;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletInputStream;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.jolira.st4j.Metric;
+import com.jolira.testing.WebServerEmulator;
 
 @SuppressWarnings("javadoc")
 public class ServerTrackerImplTest {
+    final static Logger LOG = LoggerFactory.getLogger(ServerTrackerImplTest.class);
+
     interface MyMetric {
         void setValueA(final long a);
         void setValueB(final String b);
@@ -29,7 +45,7 @@ public class ServerTrackerImplTest {
 
     @Test
     public void testGetMetric() {
-        final ServerTrackerImpl tracker = new ServerTrackerImpl("localhost:3080");
+        final ServerTrackerImpl tracker = new ServerTrackerImpl("localhost:3080", null);
         final MyMetric m1 = tracker.getMetric(MyMetric.class);
         final MyMetric m1_2 = tracker.getMetric(MyMetric.class);
         final MySecondMetric m2 = tracker.getMetric(MySecondMetric.class);
@@ -40,7 +56,7 @@ public class ServerTrackerImplTest {
         m2.setValueC(0);
         m3.setValueD("jolira");
 
-        final Map<String, Map<String, Object>> collected = tracker.getCollected();
+        final Map<String, Map<String, Object>> collected = tracker.getLocal();
         final int size = collected.size();
         final Map<String, Object> r1 = collected.get("com.jolira.st4j.impl.servertrackerimpltest$mymetric");
         final Map<String, Object> r2 = collected.get("com.jolira.st4j.impl.servertrackerimpltest$mysecondmetric");
@@ -58,4 +74,69 @@ public class ServerTrackerImplTest {
         assertEquals("jolira", v4);
     }
 
+    @Test
+    public void testSubmit() throws Exception {
+        final WebServerEmulator server = new WebServerEmulator() {
+            @Override
+            public void handle(final String target, final HttpServletRequest request, final HttpServletResponse response)
+                    throws IOException, ServletException {
+                final String contentType = request.getContentType();
+                assertEquals("/submit/metric", target);
+                assertEquals("application/json", contentType);
+
+                final ServletInputStream in = request.getInputStream();
+                final InputStreamReader _in = new InputStreamReader(in);
+                final char[] buffer = new char[2048];
+
+                _in.read(buffer);
+
+                final String body = new String(buffer);
+
+                LOG.info("body: {}", body);
+
+                if (!body.contains("com.jolira.st4j.impl.servertrackerimpltest$mymetric\":{")) {
+                    fail("body does not contain com.jolira.st4j.impl.servertrackerimpltest$mymetric: " + body);
+                }
+
+                if (!body.contains("\"thrid\":{\"valueD\":\"jolira\"}")) {
+                    fail("body does not contain thrid: " + body);
+                }
+
+                response.setContentType(contentType);
+
+                final ServletOutputStream out = response.getOutputStream();
+                final OutputStreamWriter writer = new OutputStreamWriter(out);
+
+                writer.append("true");
+                writer.close();
+            }
+        };
+
+        server.start();
+
+        final String name = server.getName();
+
+        try {
+            final ServerTrackerImpl tracker = new ServerTrackerImpl(name, new Executor(){
+                @Override
+                public void execute(final Runnable command) {
+                    command.run();
+                }});
+            final MyMetric m1 = tracker.getMetric(MyMetric.class);
+            tracker.getMetric(MyMetric.class);
+            final MySecondMetric m2 = tracker.getMetric(MySecondMetric.class);
+            final MyThirdMetric m3 = tracker.getMetric(MyThirdMetric.class);
+
+            m1.setValueA(1234567890l);
+            m1.setValueB("test");
+            m2.setValueC(0);
+            m3.setValueD("jolira");
+
+            tracker.submit();
+            tracker.submit();
+        }
+        finally {
+            server.stop();
+        }
+    }
 }
